1f27a49d51950c71d56e815405b1fa98ebd11326
Divyansh <divyanshgupta100@gmail.com>
2022-03-05 00:09:55 +0530

    a

--- server/index.js
+++ server/index.js
@@ -77,7 +77,7 @@ io.on("connection", (socket) => {
     })
 })
 
-server.listen(process.env.PORT || 3001 , () => {
+server.listen(process.env.PORT || 3000 , () => {
     console.log("SERVER RUNNING");
 });
 
